cb1e44ae7c630ad11ab455dcf3bab3e9be66d23a
mackhood <germanjugo@hotmail.com>
2021-04-10 15:58:20 -0300

    subo estructura del proyecto del backend y frontend

---
+++ Backend/src/main/java/com/example/TACS2021UTN/Tacs2021UtnApplication.java
@@ -0,0 +1,13 @@
+package com.example.TACS2021UTN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tacs2021UtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tacs2021UtnApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/TACS2021UTN/Tacs2021UtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TACS2021UTN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tacs2021UtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
