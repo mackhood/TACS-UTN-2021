38dbdd6e9c1850c48d4c386d14e7400932dddb69
gvidal <gvidal@frba.utn.edu.ar>
2021-04-13 00:26:22 -0300

    Home Route

--- Backend/src/main/java/com/example/TACS2021UTN/Tacs2021UtnApplication.java
+++ Backend/src/main/java/com/example/TACS2021UTN/Tacs2021UtnApplication.java
@@ -2,10 +2,17 @@ package com.example.TACS2021UTN;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RestController
 public class Tacs2021UtnApplication {
 
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(Tacs2021UtnApplication.class, args);
 	}
