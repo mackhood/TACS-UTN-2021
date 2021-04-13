d3805c7b0a540f46eace0dfb2d7d8bdbd75ae95a
gvidal <gvidal@frba.utn.edu.ar>
2021-04-13 01:12:42 -0300

    Backend dockerfile con ruta de prueba. Frontend hace un get y lo rechazan por cors. Corre todo con un comando de docker

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
+import axios from 'axios';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [response, setResponse] = useState(null);
+    useEffect(() => {
+        axios.get("backend:8080/")
+            .then(res => {
+                setResponse(res.data);
+                setLoading(false);
+            })
+            .catch(err => console.log(err));
+
+    }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h3>TACS-UTN-2021</h3>
+        {loading ? <>Cargando...</> : response}
+    </>
   );
 }
 
