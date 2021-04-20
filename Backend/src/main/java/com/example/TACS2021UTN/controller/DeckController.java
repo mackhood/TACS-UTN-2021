f5c875fc0622299b11062f5715e29a8d23bea708
gvidal <gvidal@frba.utn.edu.ar>
2021-04-19 23:25:56 -0300

    Get all decks

--- Backend/src/main/java/com/example/TACS2021UTN/controller/DeckController.java
+++ Backend/src/main/java/com/example/TACS2021UTN/controller/DeckController.java
@@ -2,6 +2,7 @@ package com.example.TACS2021UTN.controller;
 
 import com.example.TACS2021UTN.DTO.DeckDTO;
 import com.example.TACS2021UTN.entities.Deck;
+import com.example.TACS2021UTN.entities.Card;
 import com.example.TACS2021UTN.exceptions.DeckNotFoundException;
 import com.example.TACS2021UTN.service.deck.IDeckService;
 import org.modelmapper.ModelMapper;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 @RestController
@@ -21,11 +23,47 @@ public class DeckController {
     private ModelMapper modelMapper;
 
     @GetMapping("/decks")
-    public List<DeckDTO> getAllDecks(){
-        List<Deck> list = service.getAllDecks();
+    public List<Deck> getAllDecks(){
 
-        List<DeckDTO> mappedDeck = list.stream().map(deck -> modelMapper.map(deck, DeckDTO.class)).collect(Collectors.toList());
-        return mappedDeck;
+        List<Card> cards = new ArrayList<>();
+        List<Deck> decks = new ArrayList<>();
+
+        Card card1 = new Card();
+        card1.setName("Batman");
+        card1.setIntelligence(100);
+        card1.setSpeed(27);
+        card1.setDurability(35);
+        card1.setPower(50);
+        card1.setCombat(70);
+        card1.setId(new Long(1));
+
+        Card card2 = new Card();
+        card2.setName("Robin");
+        card2.setIntelligence(7);
+        card2.setSpeed(33);
+        card2.setDurability(25);
+        card2.setPower(30);
+        card2.setCombat(50);
+        card2.setId(new Long(2));
+
+        cards.add(card1);
+        cards.add(card2);
+
+
+        Deck deck1 = new Deck();
+        deck1.setName("Super Mazo");
+        deck1.setCardList(cards);
+
+        Deck deck2 = new Deck();
+        deck1.setName("Mazo Increible");
+        deck1.setCardList(cards);
+        decks.add(deck1);
+        decks.add(deck2);
+        return decks;
+//         List<Deck> list = service.getAllDecks();
+//
+//         List<DeckDTO> mappedDeck = list.stream().map(deck -> modelMapper.map(deck, DeckDTO.class)).collect(Collectors.toList());
+//         return mappedDeck;
     }
 
     @GetMapping("/decks/{id}")
--- Backend/src/main/java/com/example/TACS2021UTN/entities/Card.java
+++ Backend/src/main/java/com/example/TACS2021UTN/entities/Card.java
@@ -4,22 +4,32 @@ import com.example.TACS2021UTN.entities.attribute.Attribute;
 
 import javax.persistence.Entity;
 import javax.persistence.Transient;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Getter @Setter @NoArgsConstructor
 public class Card extends PersistantEntity{
 
-    @Transient
-    private List<Attribute> atributes = new ArrayList<>();
+//     @Transient
+//     private List<Attribute> atributes = new ArrayList<>();
 
 
-    public Card(){
-
-
-
-    }
-
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer strength;
+    private Integer intelligence;
+    private Integer speed;
+    private Integer durability;
+    private Integer power;
+    private Integer combat;
 }
