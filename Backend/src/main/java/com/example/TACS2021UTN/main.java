b669996ab8ddfb9cc7e0c0cee4a12b50bae49248
brian-kim98 <brianhockey98@gmail.com>
2021-05-23 12:24:54 -0300

    Filtrado al Deck por Cartas con todos los atributos, falta test

--- Backend/src/main/java/com/example/TACS2021UTN/main.java
+++ Backend/src/main/java/com/example/TACS2021UTN/main.java
@@ -4,18 +4,20 @@ import com.example.TACS2021UTN.DTO.AppearenceStatDTO;
 import com.example.TACS2021UTN.DTO.ImageDTO;
 import com.example.TACS2021UTN.DTO.PowerStatDTO;
 import com.example.TACS2021UTN.DTO.UserDTO;
+import com.example.TACS2021UTN.models.Card;
 import com.example.TACS2021UTN.models.user.Role;
 import com.example.TACS2021UTN.models.user.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
 
         ModelMapper modelMapper = new ModelMapper();
 
@@ -29,10 +31,20 @@ public class main {
 
         System.out.println(userDTO.toString());
 
+        /////////////////////////////
+        Card uselessCard = new Card("", 1,2,3,4,5,6);
 
+        System.out.println(uselessCard.correctCard());
 
 
 
     }
-
+/*
+    public boolean checkNull() throws IllegalAccessException {
+        for (Field f : getClass().getDeclaredFields())
+            if (f.get(this) != null)
+                return false;
+        return true;
+    }
+*/
 }
