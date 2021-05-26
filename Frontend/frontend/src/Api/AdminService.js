bf8060d610f6c355aead2418203b36525e6896f2
gvidal <gvidal@frba.utn.edu.ar>
2021-05-26 20:08:56 -0300

    Fix requests headers

--- Frontend/frontend/src/Api/AdminService.js
+++ Frontend/frontend/src/Api/AdminService.js
@@ -11,21 +11,30 @@ class AdminService{
     createDeck(deck, token){
 
         return axios.post(REACT_APP_DECK_REST_API_URL , this.generateBody(deck) , {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
             });
     }
 
     updateDeck(deck, token){
         return axios.put(REACT_APP_DECK_REST_API_URL + '/' + parseInt(deck.id) , this.generateBody(deck), {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        });
+            'headers':
+                {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
+        }
+        );
     }
     deleteDeck(deckId, token){
         return axios.delete(REACT_APP_DECK_REST_API_URL + '/' + parseInt(deckId) , {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
+            'headers':
+                {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
         });
     }
 }
