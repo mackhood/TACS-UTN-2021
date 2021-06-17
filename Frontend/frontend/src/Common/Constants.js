8dfd3357e4d0ef51504814ae8be91077063136e4
gvidal <gvidal@frba.utn.edu.ar>
2021-06-17 05:17:52 -0300

    Rest api url constant for axios instance

--- Frontend/frontend/src/Common/Constants.js
+++ Frontend/frontend/src/Common/Constants.js
@@ -22,3 +22,8 @@ export const REACT_APP_LOGIN_REST_API_URL =
     process.env.REACT_APP_LOGIN_REST_API_URL ?
     process.env.REACT_APP_LOGIN_REST_API_URL:
         'http://localhost:8080';
+
+export const REACT_APP_REST_API_URL =
+    process.env.REACT_APP_REST_API_URL ?
+        process.env.REACT_APP_REST_API_URL:
+        'http://localhost:8080';
