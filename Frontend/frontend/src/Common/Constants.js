79f4475e979bf86000b80da9000d62e956800df2
gvidal <gvidal@frba.utn.edu.ar>
2021-07-27 17:21:58 -0300

    Replace backend url with redirect prefix

--- Frontend/frontend/src/Common/Constants.js
+++ Frontend/frontend/src/Common/Constants.js
@@ -1,12 +1,6 @@
-export const REACT_APP_LOGIN_REST_API_URL =
-    process.env.REACT_APP_LOGIN_REST_API_URL ?
-    process.env.REACT_APP_LOGIN_REST_API_URL:
-        'http://localhost:8080';
+export const REACT_APP_LOGIN_REST_API_URL = 'api/';
 
-export const REACT_APP_REST_API_URL =
-    process.env.REACT_APP_REST_API_URL ?
-        process.env.REACT_APP_REST_API_URL:
-        'http://localhost:8080';
+export const REACT_APP_REST_API_URL ='api/'
 
 export const NEW_TURN = {
     "creatorCard": {
