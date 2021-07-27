20ec45d0311c10a7f35bd909b9fcaa8336ca981a
gvidal <gvidal@frba.utn.edu.ar>
2021-07-27 07:43:21 -0300

    Fix delete deck

--- Backend/src/main/java/com/example/TACS2021UTN/repositories/deck/IDeckRepository.java
+++ Backend/src/main/java/com/example/TACS2021UTN/repositories/deck/IDeckRepository.java
@@ -3,6 +3,8 @@ package com.example.TACS2021UTN.repositories.deck;
 import com.example.TACS2021UTN.models.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +13,8 @@ public interface IDeckRepository extends JpaRepository<Deck, Long> {
     Optional<Deck> findByName(String name);
     Optional<Deck> findById(Long id);
     //void save(Deck deck);
+    @Transactional
+    @Modifying
     @Query("delete FROM Deck d where d.id=:id")
     void deleteById(Long id);
     //Boolean deleteById(Long id);
